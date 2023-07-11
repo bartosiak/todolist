21cdfacd60daa254a23952a2921742e07aad217e
Damian <bartosiak.contact@gmail.com>
2023-07-11 22:43:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+#TODOLIST
+
+TodoList is a simple web application that allows you to create and manage your to-do lists. You can add items to the list, mark them as completed by checking the checkboxes, and remove them from the list.
+
+##Features
+
+- Create multiple to-do lists eg. todolist/Work
+- Add new items to the list
+- Remove items from the list
+
+##Technologies Used
+
+-Front-end: HTML, CSS, JavaScript
+-Back-end: Node.js, Express.js
+-Database: MongoDB
+-External Libraries: Mongoose, EJS
+
+##Installation
+To run the TodoList application locally, please follow these steps:
+
+1. Clone the repository:
+   bash:
+   git clone https://github.com/your-username/TodoList.git
+
+2. Install the dependencies:
+   npm install
+
+3. Start the application:
+   node app.js
+
+4. Open your web browser and visit http://localhost:3000 to access the application.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+Feel free to customize the content and structure of the README.md file according to your application's specific details and requirements.
---
+++ app.js
@@ -0,0 +1,169 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+//
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const uri =
+  "mongodb+srv://admin-damian:9zct1VX7DgsVrHqJ@todolist.xxrtvnd.mongodb.net/todolistDB?retryWrites=true&w=majority";
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error);
+  });
+
+const itemsSchema = {
+  name: String,
+};
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!",
+});
+const item2 = new Item({
+  name: "Hit the + button to add new item.",
+});
+const item3 = new Item({
+  name: "Hit this to delete an item.!",
+});
+
+defaultItems = [item1, item2, item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema],
+};
+
+const List = mongoose.model("List", listSchema);
+
+app.get("/", function (req, res) {
+  Item.find({})
+    .then(function (foundItems) {
+      if (foundItems.length === 0) {
+        // Dodaj trzy domyślne elementy do listy
+
+        // Zapisz domyślne elementy do bazy danych
+        return Item.insertMany([item1, item2, item3]);
+      } else {
+        // Kontynuuj renderowanie widoku z istniejącymi elementami
+        console.log(foundItems);
+        return foundItems;
+      }
+    })
+    .then(function (foundItems) {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.get("/:customListName", function (req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+
+  // Sprawdź, czy wartość name jest już użyta
+  List.findOne({ name: customListName })
+    .then(function (foundList) {
+      if (foundList) {
+        // Jeśli lista o takiej nazwie już istnieje, wykonaj odpowiednie operacje
+
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items,
+        });
+      } else {
+        // Jeśli lista o takiej nazwie nie istnieje, utwórz nową listę
+        const list = new List({
+          name: customListName,
+          items: defaultItems,
+        });
+
+        return list.save().then(function () {
+          console.log("Nowa lista została utworzona!");
+          res.redirect("/" + customListName);
+        });
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
+app.post("/", async function (req, res) {
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+  const item = new Item({
+    name: itemName,
+  });
+
+  if (listName === "Today") {
+    try {
+      await item.save();
+      res.redirect("/");
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    try {
+      const foundList = await List.findOne({ name: listName });
+      foundList.items.push(item);
+      await foundList.save();
+      res.redirect("/" + listName);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+app.post("/delete", async function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    try {
+      await Item.findByIdAndRemove(checkedItemId);
+      res.redirect("/");
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    try {
+      await List.findOneAndUpdate(
+        { name: listName },
+        { $pull: { items: { _id: checkedItemId } } }
+      );
+      res.redirect("/" + listName);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+// app.get("/work", function (req, res) {
+//   res.render("list", { listTitle: "Work List", newListItems: workItems });
+// });
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+const port = process.env.PORT || 3000; // Ustawienie portu, jeśli nie jest ustawiona zmienna środowiskowa PORT
+
+app.listen(port, function () {
+  console.log("Serwer nasłuchuje na porcie " + port);
+});
