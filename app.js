e8302c1d67fb21250ccc783abd368bc0a1ecb39e
Damian <bartosiak.contact@gmail.com>
2023-08-30 19:58:26 +0200

    Update code to remove console logs and commented-out text
    Removed unnecessary console logs and commented-out code to improve code cleanliness and readability.

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
+require("dotenv").config();
 const app = express();
 
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -39,7 +35,7 @@ const item2 = new Item({
   name: "Hit the + button to add new item.",
 });
 const item3 = new Item({
-  name: "Hit this to delete an item.!",
+  name: "<-- Hit this to delete an item.!",
 });
 
 defaultItems = [item1, item2, item3];
@@ -55,12 +51,8 @@ app.get("/", function (req, res) {
   Item.find({})
     .then(function (foundItems) {
       if (foundItems.length === 0) {
-        // Dodaj trzy domyślne elementy do listy
-
-        // Zapisz domyślne elementy do bazy danych
         return Item.insertMany([item1, item2, item3]);
       } else {
-        // Kontynuuj renderowanie widoku z istniejącymi elementami
         console.log(foundItems);
         return foundItems;
       }
@@ -76,25 +68,20 @@ app.get("/", function (req, res) {
 app.get("/:customListName", function (req, res) {
   const customListName = _.capitalize(req.params.customListName);
 
-  // Sprawdź, czy wartość name jest już użyta
   List.findOne({ name: customListName })
     .then(function (foundList) {
       if (foundList) {
-        // Jeśli lista o takiej nazwie już istnieje, wykonaj odpowiednie operacje
-
         res.render("list", {
           listTitle: foundList.name,
           newListItems: foundList.items,
         });
       } else {
-        // Jeśli lista o takiej nazwie nie istnieje, utwórz nową listę
         const list = new List({
           name: customListName,
           items: defaultItems,
         });
 
         return list.save().then(function () {
-          console.log("Nowa lista została utworzona!");
           res.redirect("/" + customListName);
         });
       }
@@ -154,16 +141,6 @@ app.post("/delete", async function (req, res) {
   }
 });
 
-// app.get("/work", function (req, res) {
-//   res.render("list", { listTitle: "Work List", newListItems: workItems });
-// });
+const port = process.env.PORT || 3000;
 
-app.get("/about", function (req, res) {
-  res.render("about");
-});
-
-const port = process.env.PORT || 3000; // Ustawienie portu, jeśli nie jest ustawiona zmienna środowiskowa PORT
-
-app.listen(port, function () {
-  console.log("Serwer nasłuchuje na porcie " + port);
-});
+app.listen(port, function () {});
